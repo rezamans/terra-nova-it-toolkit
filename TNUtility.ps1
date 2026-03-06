46652a56cfbac058574f70ecbe40384772c881ea
rezamans <62286552+rezamans@users.noreply.github.com>
2026-03-06 09:57:27 -0800

    Update TNUtility.ps1

--- TNUtility.ps1
+++ TNUtility.ps1
@@ -6,7 +6,14 @@ $repo = "https://raw.githubusercontent.com/rezamans/terra-nova-it-toolkit/main/m
 Write-Host "Loading modules..." -ForegroundColor Cyan
 
 irm "$repo/localadmin.ps1" | iex
+irm "$repo/apps.ps1" | iex
 
 Write-Host "Modules loaded." -ForegroundColor Green
 
 Ensure-LocalAdmin
+
+Install-AppIfMissing "googlechrome"
+Install-AppIfMissing "firefox"
+Install-AppIfMissing "zoom"
+Install-AppIfMissing "7zip.install"
+Install-AppIfMissing "adobereader"
