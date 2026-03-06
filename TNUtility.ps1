e8f029334c5c84e8eadef6e03ff34edf47ae2ac0
rezamans <62286552+rezamans@users.noreply.github.com>
2026-03-06 10:07:18 -0800

    Update TNUtility.ps1

--- TNUtility.ps1
+++ TNUtility.ps1
@@ -1,17 +1,3 @@
-Write-Host "Terra Nova IT Utility Started..." -ForegroundColor Cyan
-Write-Host "Running by Reza Mansouri" -ForegroundColor Yellow
-
-$repo = "https://raw.githubusercontent.com/rezamans/terra-nova-it-toolkit/main/modules"
-
-Write-Host "Loading modules..." -ForegroundColor Cyan
-
-irm "$repo/localadmin.ps1" | iex
-irm "$repo/apps.ps1" | iex
-
-Write-Host "Modules loaded." -ForegroundColor Green
-
-Ensure-LocalAdmin
-
 Install-AppIfMissing "googlechrome"
 Install-AppIfMissing "firefox"
 Install-AppIfMissing "zoom"
