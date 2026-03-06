6e3207d920153426c5a0ed57d65040516a978bde
rezamans <62286552+rezamans@users.noreply.github.com>
2026-03-06 10:08:57 -0800

    Update TNUtility.ps1

--- TNUtility.ps1
+++ TNUtility.ps1
@@ -1,5 +1,27 @@
-Install-AppIfMissing "googlechrome"
-Install-AppIfMissing "firefox"
-Install-AppIfMissing "zoom"
-Install-AppIfMissing "7zip.install"
-Install-AppIfMissing "adobereader"
+Write-Host "Terra Nova IT Utility Started..." -ForegroundColor Cyan
+Write-Host "Running by Reza Mansouri" -ForegroundColor Yellow
+
+$repo = "https://raw.githubusercontent.com/rezamans/terra-nova-it-toolkit/main/modules"
+
+Write-Host "Loading modules..." -ForegroundColor Cyan
+
+irm "$repo/localadmin.ps1" | iex
+irm "$repo/apps.ps1" | iex
+
+Write-Host "Modules loaded." -ForegroundColor Green
+
+# 1) Ensure Local Admin first
+Ensure-LocalAdmin
+
+# 2) Install standard apps only if missing
+Install-AppIfMissing "Google Chrome" "googlechrome" "C:\Program Files\Google\Chrome\Application\chrome.exe"
+
+Install-AppIfMissing "Firefox" "firefox" "C:\Program Files\Mozilla Firefox\firefox.exe"
+
+Install-AppIfMissing "Zoom" "zoom" "C:\Program Files\Zoom\bin\Zoom.exe"
+
+Install-AppIfMissing "7-Zip" "7zip.install" "C:\Program Files\7-Zip\7z.exe"
+
+Install-AppIfMissing "Adobe Acrobat" "adobereader" ""
+
+Write-Host "Base deployment section completed." -ForegroundColor Green
